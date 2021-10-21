b840479b9957ddd234e9617880fa65c2d3551d75
Vitaliya_keks <73116690+VitaliiPysyniuk@users.noreply.github.com>
2021-10-21 23:00:19 +0300

    Created models.

--- backend/apps/group/models.py
+++ backend/apps/group/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from ..user.models import UserModel
+
+
+class GroupModel(models.Model):
+    class Meta:
+        db_table = 'groups'
+
+    name = models.CharField(max_length=40, unique=True)
+    description = models.CharField(max_length=255)
+
+
+class UserGroupModel(models.Model):
+    class Meta:
+        db_table = 'groups_users'
+
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE, related_name='groups')
+    group = models.ForeignKey(GroupModel, on_delete=models.CASCADE, related_name='users')
+
